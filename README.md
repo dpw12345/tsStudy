4db54071001bcef38f062544123115aad8a27456
dingpeiwen <1529429155@qq.com>
2023-07-06 11:04:02 +0800

    安装node环境下可以进行ts编译的包

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # tsStudy
 study by myself
+
+在node环境直接编译ts文件，下载两个包 npm i ts-node -g //// npm i @types/node -D 然后运行 ts-node ts文件名
---
+++ ts项目/1-基本数据类型定义.ts
@@ -0,0 +1,8 @@
+// 最简单的定义
+let str:string = 'str';
+let num:number = 123
+let und:undefined = undefined
+let nul:null = null
+let obj:object = {name:'name'}
+let arr:Array<number> = [1]
+console.log(123);
